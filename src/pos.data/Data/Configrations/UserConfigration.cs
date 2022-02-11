e4d62bd33a4affb36ab48057b47b033d580d7505
Quang Phan <quang.phan@tallygroup.com.au>
2022-02-11 21:49:32 +0700

    add user entity

---
+++ src/pos.data/Data/Configrations/UserConfigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using pos.core.Entities;
+
+namespace pos.core.Data.Configrations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users", "account");
+
+            builder.Property(x => x.Username)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.HasIndex(x => x.Username)
+                .IsUnique();
+
+            builder.Property(x => x.FirstName).HasMaxLength(250);
+            builder.Property(x => x.LastName).HasMaxLength(250);
+            builder.Property(x => x.Password).HasMaxLength(250);
+            builder.Property(x => x.Email).HasMaxLength(250);
+        }
+    }
+}
--- src/pos.data/Data/TenantDbContext.cs
+++ src/pos.data/Data/TenantDbContext.cs
@@ -13,12 +13,14 @@ namespace pos.core.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
         }
     }
 }
