10d7b6b1bc1d1a5a962fd4b110dba654b7775a9b
Quang Phan <quang.phan@tallygroup.com.au>
2022-02-15 21:03:35 +0700

    update action buttons background

--- src/pos.web/ClientApp/src/app/pages/pos/pos.component.ts
+++ src/pos.web/ClientApp/src/app/pages/pos/pos.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from "./models";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-pos",
--- src/pos.web/ClientApp/src/app/pages/pos/pos.module.ts
+++ src/pos.web/ClientApp/src/app/pages/pos/pos.module.ts
@@ -2,24 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PosRoutingModule } from './pos-routing.module';
-import { PosComponent } from './pos.component';
-import { PaymentComponent } from './payment/payment.component';
-import { PosHeaderComponent } from './pos-header/pos-header.component';
-import { PosPaymentComponent } from './pos-payment/pos-payment.component';
-import { PosCartComponent } from './pos-cart/pos-cart.component';
-
+import { PosComponent } from "./pos.component";
+import { PosHeaderComponent } from "./pos-header/pos-header.component";
+import { PosPaymentComponent } from "./pos-payment/pos-payment.component";
+import { PosCartComponent } from "./pos-cart/pos-cart.component";
 
 @NgModule({
   declarations: [
     PosComponent,
-    PaymentComponent,
     PosHeaderComponent,
     PosPaymentComponent,
-    PosCartComponent
+    PosCartComponent,
   ],
-  imports: [
-    CommonModule,
-    PosRoutingModule
-  ]
+  imports: [CommonModule, PosRoutingModule],
 })
-export class PosModule { }
+export class PosModule {}
