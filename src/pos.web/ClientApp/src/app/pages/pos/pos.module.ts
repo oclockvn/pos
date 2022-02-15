ae048ccdd980d47d56755716d21245d294acd63e
Quang Phan <quang.phan@tallygroup.com.au>
2022-02-15 20:43:02 +0700

    split payment component

--- src/pos.web/ClientApp/src/app/pages/pos/pos.module.ts
+++ src/pos.web/ClientApp/src/app/pages/pos/pos.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PosRoutingModule } from './pos-routing.module';
 import { PosComponent } from './pos.component';
+import { PaymentComponent } from './payment/payment.component';
 
 
 @NgModule({
   declarations: [
-    PosComponent
+    PosComponent,
+    PaymentComponent
   ],
   imports: [
     CommonModule,
