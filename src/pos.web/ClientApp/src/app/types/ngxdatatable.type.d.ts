25a9af845534a25e74b178efbe38c6e7c25fea47
Quang Phan <quang.phan@tallygroup.com.au>
2022-03-08 22:41:01 +0700

    remove ngx column type def

--- src/pos.web/ClientApp/src/app/types/ngxdatatable.type.d.ts
+++ src/pos.web/ClientApp/src/app/types/ngxdatatable.type.d.ts
@@ -1,28 +1,3 @@
-import { PipeTransform, TemplateRef } from "@angular/core";
-
-export declare type NgxColumn = {
-  name: string;
-  prop: string;
-  flexGrow: number;
-  minWidth: number;
-  maxWidth: number;
-  width: number;
-  resizeable: boolean;
-  // comparator: any;
-  sortable: boolean;
-  draggable: boolean;
-  canAutoResize: boolean;
-  cellTemplate: TemplateRef<any>;
-  headerTemplate: TemplateRef<any>;
-  checkboxable: boolean;
-  headerCheckboxable: boolean;
-  headerClass: string | Function;
-  cellClass: string | Function;
-  frozenLeft: boolean;
-  frozenRight: boolean;
-  pipe: PipeTransform;
-};
-
 export declare type PageInfo = {
   count: number;
   pageSize: number;
